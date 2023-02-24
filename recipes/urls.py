4157e95e6a8f60c155ec6ffde13cc3baa517a2e5
unknown <andre_luiz29@hotmail.com>
2023-02-24 00:22:10 -0300

    init

---
+++ recipes/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from recipes.views import contato, home, sobre
+
+urlpatterns = [
+    path('', home),  # HOME
+    path('sobre', sobre),  # SOBRE
+    path('contato', contato),  # CONTATO
+]
