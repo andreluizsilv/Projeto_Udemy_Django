494f1b52dfa456abacc7f61828d2295de76ac414
unknown <andre_luiz29@hotmail.com>
2023-02-27 12:52:31 -0300

    templates ok

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from recipes.views import contato, home, sobre
 
 urlpatterns = [
-    path('', home),  # HOME
-    path('sobre', sobre),  # SOBRE
-    path('contato', contato),  # CONTATO
+    path('', home),  # HOME/
+    path('sobre/', sobre),  # SOBRE/
+    path('contato/', contato),  # CONTATO/
 ]
